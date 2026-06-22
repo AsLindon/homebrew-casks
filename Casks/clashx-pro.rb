824f638318c8d625153da3fed91e179c299dd074
ø <62425376+AsLindon@users.noreply.github.com>
2026-06-22 16:42:23 +0800

    Add ClashX Pro cask

---
+++ Casks/clashx-pro.rb
@@ -0,0 +1,22 @@
+cask "clashx-pro" do
+  version "1.139.0"
+  sha256 "e6f3e51b0bc8e7a4a3a4c0f14cee327a7e62e924c763d9cfd3fd6a40a52f01a9"
+
+  url "https://github.com/ClashX-Pro/ClashX/releases/download/#{version}/ClashX.dmg"
+  name "ClashX"
+  desc "Rule-based proxy client for macOS"
+  homepage "https://github.com/ClashX-Pro/ClashX"
+  
+  livecheck do
+    skip "Handled by GitHub Actions"
+  end
+  
+  app "ClashX.app"
+
+  zap trash: [
+    "~/.config/clash",
+    "~/Library/Application Support/ClashX",
+    "~/Library/Caches/com.west2online.ClashX",
+    "~/Library/Preferences/com.west2online.ClashX.plist",
+  ]
+end
