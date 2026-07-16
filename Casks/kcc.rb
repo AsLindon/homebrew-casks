b12171790bb69b10c5743fb41eee74ba1e1a0098
ø <62425376+AsLindon@users.noreply.github.com>
2026-07-16 19:12:40 +0200

    Add complete KCC cask

--- Casks/kcc.rb
+++ Casks/kcc.rb
@@ -1,7 +1,7 @@
 cask "kcc" do
   arch arm: "arm", intel: "i386"
 
-  version "10.2.0"
+  version "10.4.0"
   sha256 :no_check
 
   url "https://github.com/ciromattia/kcc/releases/download/v#{version}/kcc_macos_#{arch}_#{version}.dmg",
@@ -16,3 +16,13 @@ cask "kcc" do
   end
 
   depends_on macos: ">= :sonoma"
+
+  app "Kindle Comic Converter.app"
+
+  zap trash: [
+    "~/Library/Application Support/Kindle Comic Converter",
+    "~/Library/Caches/Kindle Comic Converter",
+    "~/Library/Preferences/Kindle Comic Converter.plist",
+    "~/Library/Saved Application State/Kindle Comic Converter.savedState",
+  ]
+end
