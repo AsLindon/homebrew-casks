42baf4b5f0675b5bcbdd246dff1d6ebe7cbb2a85
ø <62425376+AsLindon@users.noreply.github.com>
2026-07-16 19:18:04 +0200

    Fix KCC version and checksums

--- Casks/kcc.rb
+++ Casks/kcc.rb
@@ -1,8 +1,9 @@
 cask "kcc" do
   arch arm: "arm", intel: "i386"
 
-  version "10.4.0"
-  sha256 :no_check
+  version "10.2.0"
+  sha256 arm:   "3aa84049ed9b7d46b5bbb66042a96a384a22d1658c9a9336f4d0a90b2dde5075",
+         intel: "05cf5b8ec3f9d319b27e05243e551092e3e5729b7c2c6226decdce9fa42f5c29"
 
   url "https://github.com/ciromattia/kcc/releases/download/v#{version}/kcc_macos_#{arch}_#{version}.dmg",
       verified: "github.com/ciromattia/kcc/"
