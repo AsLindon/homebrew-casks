22d7da0933acb9c52e56d73369e3d8d8fa0dc0f3
ø <62425376+AsLindon@users.noreply.github.com>
2026-09-16 21:41:53 +0200

    Remove deprecated verified parameter from kcc.rb

--- Casks/kcc.rb
+++ Casks/kcc.rb
@@ -5,8 +5,7 @@ cask "kcc" do
   sha256 arm:   "f8f8e3250c8f6e9aca1f73e2837faa8f576657d6a21ac7a80691d006554ea7ec",
          intel: "c78450dfcfede23d7677c2594fc10996d5b4fd3715f27a52079f9d2e926e0a1f"
 
-  url "https://github.com/ciromattia/kcc/releases/download/v#{version}/kcc_macos_#{arch}_#{version}.dmg",
-      verified: "github.com/ciromattia/kcc/"
+  url "https://github.com/ciromattia/kcc/releases/download/v#{version}/kcc_macos_#{arch}_#{version}.dmg"
   name "Kindle Comic Converter"
   desc "Convert comics and manga for e-book readers"
   homepage "https://github.com/ciromattia/kcc"
